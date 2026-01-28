f4f39a6f52333d2c58f10d88008d39fe0c25ebf2
ValeriaErokhina <valeria.ue@mail.ru>
2026-01-28 20:51:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//первое задание
 document.querySelector('.play_btn').addEventListener('click', function(e) {
   e.target.closest('.play_btn').classList.toggle('play_btn--toggle');
 })
+//второе задание
+const button = document.querySelector("#btn");
+
+button.addEventListener('click', () => {
+   alert(`Ширина: ${window.screen.width}. Высота: ${window.screen.height}.`);
+});
