2b4211ba299d0bc7057f8a91b9bd687749e731c8
ValeriaErokhina <valeria.ue@mail.ru>
2026-02-01 18:01:13 +0300

    Remove button click event for screen dimensions alert
    Removed second task button click event listener and associated code.

--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 document.querySelector('.play_btn').addEventListener('click', function(e) {
   e.target.closest('.play_btn').classList.toggle('play_btn--toggle');
 })
-//второе задание
-const button = document.querySelector("#btn");
 
-button.addEventListener('click', () => {
-   alert(`Ширина: ${window.screen.width}. Высота: ${window.screen.height}.`);
-});
